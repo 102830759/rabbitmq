0bab1078f7148256ca915f186d933e1aca9f8d7e
huy <huyue@hdsxtech.com>
2017-11-07 11:03:32 +0800

    dsfa

---
+++ src/main/java/com/hdsx/rabbitmq/config/BaseConfig.java
@@ -0,0 +1,12 @@
+package com.hdsx.rabbitmq.config;
+
+/**
+ * Created by zhaod on 2017/8/4.
+ */
+public  class  BaseConfig {
+    protected static final String APP_KEY ="f19c5462045ae94eefb44563";
+    protected static final String MASTER_SECRET = "6de11ca30113c0cda42074c2";
+    //未知
+    protected static final String DEV_KEY = "8885227f9e44358a59aa0880";
+    protected static final String DEV_SECRET = "281399a95fa3be40645f0558";
+}
